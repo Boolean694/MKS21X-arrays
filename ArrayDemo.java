4a12b27fa82df56a02a9537a15d3b4a00a332e12
Julian Wong <jwong01@stuy.edu>
2018-09-27 22:02:09 -0400

    finish problem 1

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -22,17 +22,23 @@ public class ArrayDemo {
 	}	
 	
 	//1.
-	/*public static int countZeros2D(int[][] a) {
+	public static int countZeros2D(int[][] a) {
 		int c = 0;
 		for(int sd = 0; sd < a.length; sd++) {
-			
+			for(int bs = 0; bs < a[sd].length; bs++) {
+				if(a[sd][bs] == 0){c++;}
+				else{c += 0;}
+			}	
 		}	
-	}	*/
+		return c;
+	}	
 	
     public static void main(String[] args) {
         int[] aaa = {1,3,5,7,9};
 		int[][] bbb = {{1,2,3},{4,5,6,7},{8,9},{0}};
+		int[][] ccc = {{1,5,8,0},{3,5,0,4,3,0},{0,1,0,8}};
 		printArray(aaa);
 		printArray(bbb);
+		System.out.println(countZeros2D(ccc));
     }
 }
